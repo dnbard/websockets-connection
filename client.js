aaba9bd872d6604e39886089e0c43a6cc42a6d2a
Alex Bardanov <dnbard@gmail.com>
2017-01-20 15:59:21 +0200

    server and client improvements

--- client.js
+++ client.js
@@ -10,18 +10,61 @@ var usersCounter = 0;
 
 function openConnection(index){
     const ws = new WebSocket(path);
+    var interval = null;
 
-    ws.on('open', () => ws.send(`Hello from user(index=${index})`));
-    ws.on('message', data => console.log(data));
-    ws.on('close', () => usersCounter--);
+    ws.on('open', () => {
+        ws.send(`Hello from user(index=${index})`);
+        interval = setInterval(() => {
+            ws.send(someData);
+        }, Math.ceil(Math.random() * 10000) + 5000);
+    });
+
+
+    ws.on('close', () => {
+        usersCounter--;
+        clearInterval(interval);
+    });
+}
+
+const someData = generateRandomData(4096);
+
+function generateRandomData(size){
+    const chars = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const len = chars.length;
+    const random_data = [];
+
+    while (size--) {
+        random_data.push(chars[Math.random()*len | 0]);
+    }
+
+    return random_data.join('');
+}
+
+function formatMemoryOutput(data){
+    return Math.round(data / 1000000);
+}
+
+function reportMemoryUsage(){
+    const memory = process.memoryUsage();
+    const used = formatMemoryOutput(memory.rss);
+
+    process.stdout.write('\033c');
+    console.log(`Memory used: ${used} MB`);
 }
 
 setInterval(() => {
     if (usersCounter > limit){
-        return;
+        return reportMemoryUsage();
     }
 
     for(var i = 0; i < CONNECTION_RATE; i ++){
         openConnection(usersCounter++);
     }
+    reportMemoryUsage();
 }, CONNECTION_TIMEOUT);
+
+setInterval(() => {
+    if (typeof global.gc === 'function'){
+        gc();
+    }
+}, 30000)
--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const WebSocketServer = require('uws').Server;
 const uuid = require('node-uuid').v4;
+const BASE_PORT = 3000;
+const numberOfServers = process.argv[2] || 4;
+const portsOpened = [];
 
-const wss = new WebSocketServer({ port: 3000 });
+const servers = Array.from(Array(numberOfServers).keys()).map((e, i) => {
+    const port = BASE_PORT + i * 1000;
+    const wss = new WebSocketServer({ port: port });
+    wss.on('connection', onConnection);
+
+    console.log(`Created WS server on port=${port}`);
+
+    portsOpened.push(port);
+    return wss;
+});
+
 const connections = new Map();
 var connected = 0;
+var messagesReceived = 0;
+var bytesReceived = 0;
+
+function onMessage(payload){
+    messagesReceived ++;
+    bytesReceived += (payload || '').length;
+    // console.log(JSON.stringify(payload), null, 2);
+    // process.exit(1);
+}
 
 function onConnection(ws){
     const id = uuid();
@@ -15,6 +37,8 @@ function onConnection(ws){
     connected++;
 
     ws.on('close', onClose);
+
+    ws.on('message', onMessage);
 }
 
 function onClose(){
@@ -29,11 +53,17 @@ function formatMemoryOutput(data){
 function reportMemoryUsage(){
     const memory = process.memoryUsage();
     const used = formatMemoryOutput(memory.rss);
+    const traffic = bytesReceived > 1000000000 ? (Math.ceil(bytesReceived / 10000000) / 100 + 'GB') :
+        bytesReceived > 1000000 ? (Math.ceil(bytesReceived / 100000) / 10 + 'MB') :
+        bytesReceived > 1000 ? (Math.ceil(bytesReceived / 1000) + 'KB') :
+        bytesReceived + 'b';
 
+
+    process.stdout.write('\033c');
+    console.log(`Opened ports: ${portsOpened.join(', ')}`);
     console.log(`Users connected: ${connected}, memory used: ${used} MB`);
+    console.log(`Messages received: ${messagesReceived}, ${traffic}`);
 }
-
-wss.on('connection', onConnection);
 
 reportMemoryUsage();
 setInterval(reportMemoryUsage, 1000);
